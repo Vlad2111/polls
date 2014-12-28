1200c4b7aafbcfa77914a11684f37c4c82bd2242
AlekseiPorandaikin <porandusy@mail.ru>
2014-12-28 11:48:47 +0400

    new commit

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -74,12 +74,35 @@
                                     }
                                     
                                 }
+                                
+                                class add_config{
+                                    
+                                    public function creat_txt($text){
+                                        $fp=fopen('project_duke.conf', 'w+');
+                                        fwrite($fp, $text);
+                                        fclose($fp);
+                                    }
+                                    public function create_text(){
+                                        $text="##Project Dike \n"
+                                                . "##Configuration for database postgresql \n"
+                                                . "##Type\t Host\t Port\t DBname\t User\t Password\n"
+                                                . "PostgreSQL\t localhost\t 5432\t dike\t postgres\t 1";      
+                                        $this->creat_txt($text);
+                                    }                                 
+                                }
+                                class work_with_conf {
+                                    
+                                }
                             $qw=new auth('Стас', 3);
                             echo $qw->auth_user();   
                             
                             echo "<br>";
                             $re= new BD();
                             echo $re->user;
+                            
+                            echo "<br>";
+                            $rw= new add_config();
+                            $rw->create_text();
                         ?>
 		</body>
 </html>
