f7805fb7bbcfe634bc9ebe1d00f52fdd93db3e52
AlekseiPorandaikin <porandusy@mail.ru>
2015-01-24 11:48:23 +0400

    Исправлено офрмление.

--- primer.php
+++ primer.php
@@ -3,22 +3,19 @@
 		<body>
 <?php
  try{ 
-include 'class_db.php';
-include 'class_auth.php';
-$db=DB::getInstance();
-$query= $db->getQueryDb('last_name, first_name', 'alluser', 'id_user = $1', array(1));
-echo $db->getFetchResult($query);
-echo "<hr>";
-$q= new Auth('Иван', 1);
-echo $q->getAuthUser();
-
-
-    
-      }
-                            catch (Exception $e){
-                                $error= $e->getMessage().'. Строка '.$e->getLine().': '. ' ('. $e->getFile().')';
-                                echo $error;                            
-                            }
+    include 'class_db.php';
+    include 'class_auth.php';
+    $db=DB::getInstance();
+    $query= $db->getQueryDb('last_name, first_name', 'alluser', 'id_user = $1', array(1));
+    echo $db->getFetchResult($query);
+    echo "<hr>";
+    $q= new Auth('Иван', 1);
+    echo $q->getAuthUser();
+}
+catch (Exception $e){
+    $error= $e->getMessage().'. Строка '.$e->getLine().': '. ' ('. $e->getFile().')';
+    echo $error;                            
+}
 ?>
 
                     	</body>
