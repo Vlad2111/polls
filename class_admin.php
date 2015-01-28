8c561eecbe6ae93e0c23215f3bfa7f6c72500de3
AlekseiPorandaikin <porandusy@mail.ru>
2015-01-28 10:44:43 +0400

    Изменён класс Admin

--- class_admin.php
+++ class_admin.php
@@ -23,23 +23,27 @@ class Admin{
             }
             $str_values.="'$array_values[$i]', ";    
         }
-        $this->db->InsertDb("alluser", $str_values);        
+        $this->db->insertDb("alluser", $str_values);        
         $this->log->info('Добавлен новый пользователь '.$array_values[5]);
     }    
-    public function editUser($id_user,$array_name, $array_values){
+    public function editUser($id_user,$array_name, $array_values, $id_role=null){
         $query="$array_name[0]='$array_values[0]', $array_name[1]='$array_values[1]',"
                 . "$array_name[2]='$array_values[2]', $array_name[3]='$array_values[3]',"
                 . "$array_name[4]='$array_values[4]', $array_name[5]='$array_values[5]',"
                 . "$array_name[6]='$array_values[6]' where id_user=$id_user;";
-        $this->db->updateDb('alluser', $query);        
+        $this->db->updateDb('alluser', $query);
+        if($id_role!=null){
+            $this->editRole($id_user, $id_role);
+        }
     }
     public function deleteUser($id_user){
         $query="id_user=$id_user";
         $this->db->deleteDb('alluser', $query); 
+        $this->deleteRole($id_user);
     }
     public function addRole($id_user, $id_role){
         $str_values="$id_user, $id_role";
-        $this->db->InsertDb("role_user", $str_values);
+        $this->db->insertDb("role_user", $str_values);
     }
     public function editRole($id_user, $id_role){
         $query="id_role=$id_role where id_user=$id_user;";
