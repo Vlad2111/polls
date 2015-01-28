d35bf3b3f9f3a342bf459657adbe68553a2864a3
AlekseiPorandaikin <porandusy@mail.ru>
2015-01-28 10:32:14 +0400

    Изменён класс Admin

--- class_admin.php
+++ class_admin.php
@@ -1,49 +1,53 @@
 <?php
- class admin{
+include_once 'class_db.php';
+include_once 'Log4php/Logger.php';
+    Logger::configure('setting/config.xml');
+    LoggerNDC::push("Some Context");
+class Admin{
     public $id_admin;
-    public $BD;
+    public $db;
+    public $log;
     
-    public function __construct($id_admin){//Получаем на вход ид админа
-        $this->id_admin= $id_admin;
-        $this->BD= new BD();
+    public function __construct(){
+        $this->db=DB::getInstance();
+        $this->log= Logger::getLogger(__CLASS__);
+        $this->id_admin=$id_admin;
     }
     
-    public function all_users(){ //Выводим всех пользователей кроме админа в виде массива                                   
-        $query_admin= $this->BD;
-        $array_users = $query_admin->result_array("alluser");
-        $key=0;
-        for($i=0;$i<count($array_users);$i++){
-            if($array_users[$i][0]!=$this->id_admin){
-                for($p=0;$p<count($array_users[0]);$p++){                                              
-                    $users[$key][$p]=$array_users[$i][$p];
-                    $key++;
-                }                                                
-            }else if(!$key==0) $key--; continue;
+    public function addUser($array_values){ 
+        $str_values="nextval('id'),";
+        for($i=0; $i<count($array_values);$i++){//составляем строку из значений для запроса
+            if($i==count($array_values)-1){
+                $str_values.="'$array_values[$i]'";
+                break;                
+            }
+            $str_values.="'$array_values[$i]', ";    
         }
-        return $users;
+        $this->db->InsertDb("alluser", $str_values);        
+        $this->log->info('Добавлен новый пользователь '.$array_values[5]);
+    }    
+    public function editUser($id_user,$array_name, $array_values){
+        $query="$array_name[0]='$array_values[0]', $array_name[1]='$array_values[1]',"
+                . "$array_name[2]='$array_values[2]', $array_name[3]='$array_values[3]',"
+                . "$array_name[4]='$array_values[4]', $array_name[5]='$array_values[5]',"
+                . "$array_name[6]='$array_values[6]' where id_user=$id_user;";
+        $this->db->updateDb('alluser', $query);        
     }
-    
-     public function delete_user($id_user){
-        $qu="delete from alluser where id_user=".$id_user;
-        $delete_user=$this->BD;                                       
-        return $delete_user->query_db($qu);                                       
+    public function deleteUser($id_user){
+        $query="id_user=$id_user";
+        $this->db->deleteDb('alluser', $query); 
     }
-                                    
-    public function edit_user($id_user, $cell, $value){
-        $qu="update alluser set ".$cell."='" .$value."' where id_user=".$id_user;
-        $edit_user= $this->BD;
-        $edit_user->query_db($qu);
+    public function addRole($id_user, $id_role){
+        $str_values="$id_user, $id_role";
+        $this->db->InsertDb("role_user", $str_values);
     }
-    public function add_user($last_name, $first_name, $patronymic, $type=null, $email=null){
-        if(isset($last_name) && isset($first_name) && isset($patronymic)){
-            $qu="insert into alluser values ('".$last_name."', '".$first_name."', '".$patronymic."', '".$type."', '".$email."');";
-            $add_user=new BD();
-            $add_user->query_db($qu);
-            
-        }
-        else return false;
+    public function editRole($id_user, $id_role){
+        $query="id_role=$id_role where id_user=$id_user;";
+        $this->db->updateDb('role_user', $query);
     }
-                                    
-                                    
+    public function deleteRole($id_user){
+        $query="id_user=$id_user";
+        $this->db->deleteDb('role_user', $query);         
+    }    
 }       
 ?>
