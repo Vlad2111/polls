16319ebc875bc47e9fb03bc282a841885e56518f
AlekseiPorandaikin <porandusy@mail.ru>
2015-02-24 16:19:18 +0400

    Добавлен комментарий к коду

--- primer.php
+++ primer.php
@@ -9,10 +9,8 @@ include_once 'model/MUser.php';
 $user= new MUser();
 $user->setIdUser(1);
 $admin=new AdministrationDAO();
-var_dump($admin->getListQuiz());
-var_dump($admin->deleteUser($user));
-$users=new UserDAO();
-$users->deleteUser($user);
+var_dump($admin->getListQuiz()); //   1) Отоброжение списка тестов (Класс AdministrtionDAO)
+var_dump($admin->deleteUser($user));//2) Удаление пользователя (метод наследуется из UserDAO)
  }
 
 catch (Exception $e){
